39405a4ff0694cc4c9e6876bbda63f3790680fe3
shahzeb syed <shahzeb1500@outlook.com>
2020-07-29 23:57:27 +0100

    Updates

---
+++ precache-manifest.4ff39e1d3471289fd903b46e97fbf652.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4572cb4fe746ff8b2f5cc7035cac080e",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "ae1fcf320727cb71157c",
+    "url": "/monsters-roledex/static/css/main.160663ca.chunk.css"
+  },
+  {
+    "revision": "045c2ed955cec24ea193",
+    "url": "/monsters-roledex/static/js/2.6931aef1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-roledex/static/js/2.6931aef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae1fcf320727cb71157c",
+    "url": "/monsters-roledex/static/js/main.291f5549.chunk.js"
+  },
+  {
+    "revision": "45d0ad90f40751d49e4c",
+    "url": "/monsters-roledex/static/js/runtime-main.4f7178d1.js"
+  }
+]);
